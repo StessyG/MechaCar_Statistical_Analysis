ce2fb6f37e81f1c4a76ceb0b7d38671050a8f1d8
Stessy Iriho <stessyiriho@gmail.com>
2022-04-06 23:22:05 -0500

    updating the script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,5 @@
 setwd("~/ut/modules/MechaCar_Statistical_Analysis")
-mechacar_mpg <- read_csv('MechaCar_mpg.csv') #import dataset
+mechacar_mpg <- read.csv('MechaCar_mpg.csv') #import dataset
 library(jsonlite)
 library(tidyverse)
 ?lm
@@ -7,7 +7,7 @@ lm_mechacar_mpg <- lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_
 lm_mechacar_mpg
 summary_lm_mechacar_mpg <- summary(lm_mechacar_mpg)
 summary_lm_mechacar_mpg
-suspension_coil_df <- read_csv('Suspension_Coil.csv') #import dataset
+suspension_coil_df <- read.csv('Suspension_Coil.csv') #import dataset
 total_summary <- suspension_coil_df %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') #create summary table
 total_summary
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') #create summary table
